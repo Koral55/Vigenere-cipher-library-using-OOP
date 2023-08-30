54be26e69dd216d939f237505ee1dfecdae2c18e
Koral55 <64688737+Koral55@users.noreply.github.com>
2023-08-30 16:00:56 +0200

    Update Classes.py
    Optimized code, key must now be entered

--- Classes.py
+++ Classes.py
@@ -3,22 +3,17 @@ from Alphabet_Table import alphabet_table
 
 
 class Key:
-    def __init__(self):
-        pass
-
-    def create_key_value(self, text):
+    def __init__(self, text, value):
         self.text = text
-        length = len(self.text)
-        key_list = []
-        for x in range(length):
-            if self.text[x] != " ":
-                x = random.choice(alphabet_table)
-                key_list.append(x)
-            else:
-                key_list.append(" ")
-        self.value = ''.join(key_list)
+        self.value = value
 
-
+    def key_correction(self):
+        while len(self.value) < len(self.text) - self.text.count(" "):
+            for x in range(len(self.text) - self.text.count(" ") - len(self.value)):
+                self.value = self.value + self.value[x]
+        for x in range(len(self.text)):
+            if self.text[x] == " ":
+                self.value = self.value[:x] + " " + self.value[x:]
 
 
 class Vigenere:
@@ -26,50 +21,41 @@ class Vigenere:
         pass
 
     @classmethod
-    def code(cls, text):
-        text_list = list(text)
-        key = Key()
-        key.create_key_value(text)
-        print(f"Key value: {key.value}")
-        key_list = list(key.value)
-        global coded_message_list
-        coded_message_list = []
+    def code(cls, text, key):
+        code = ""
+        key = Key(text=text, value=key)
+        key.key_correction()
         for number in range(len(text)):
-            if text_list[number] == " " and key_list[number] == " ":
-                coded_message_list.append(" ")
+            if text[number] == " " and key.value[number] == " ":
+                code = code + " "
             else:
                 for x in range(26):
-                    if alphabet_table[x * 26] == key_list[number]:
-                        global text_letter_position
+                    if alphabet_table[x * 26] == text[number]:
                         text_letter_position = x * 26
                         break
                 for y in alphabet_table:
-                    if y == text_list[number]:
-                        global key_letter_position
+                    if y == key.value[number]:
                         key_letter_position = alphabet_table.index(y)
                         break
-                coded_letter_position = text_letter_position + key_letter_position
-                coded_letter = alphabet_table[coded_letter_position]
-                coded_message_list.append(coded_letter)
-        print(f"Coded message: {''.join(coded_message_list)}")
+                code_letter_position = text_letter_position + key_letter_position
+                code = code + alphabet_table[code_letter_position]
+        return code
+
 
     @classmethod
     def decode(cls, code, key):
-        code = list(code)
-        key = list(key)
-        text_message_list = []
+        text = ""
+        key = Key(text=code, value=key)
+        key.key_correction()
         for number in range(len(code)):
-            if key[number] == " " and code[number] == " ":
-                text_message_list.append(" ")
+            if key.value[number] == " " and code[number] == " ":
+                text = text + " "
             else:
-                key_letter_position = alphabet_table.index(key[number])
+                key_letter_position = alphabet_table.index(key.value[number])
                 for z in range(26):
                     if alphabet_table[z * 26 + key_letter_position] == code[number]:
-                        global coded_letter_poisition
-                        coded_letter_position = z * 26 + key_letter_position
+                        code_letter_position = z * 26 + key_letter_position
                         break
-                text_letter_position = coded_letter_position - key_letter_position
-                text_letter = alphabet_table[text_letter_position]
-                text_message_list.append(text_letter)
-        print(f"Uncoded message: {''.join(text_message_list)}")
-
+                text_letter_position = code_letter_position - key_letter_position
+                text = text + alphabet_table[text_letter_position]
+        return text
